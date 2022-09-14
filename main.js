922652d232b7d59c3d9ce7f90d64bbc9bcc2f83b
Phongsavanh Mongkhonvilay <pmongkho12@gmail.com>
2022-09-13 21:11:51 -0500

    add files

---
+++ main.js
@@ -0,0 +1,31 @@
+//Question 1 & Question 2
+let myNumber = 5
+console.log("myNumber",typeof myNumber)
+
+//Question 3
+let myOtherNumber = 5
+
+//Question 4
+let myTotal = myNumber * myOtherNumber
+
+//Question 5
+let myName = "Phongsavanh Mongkhonvilay"
+let myCombo = `${myNumber + myOtherNumber}`
+console.log(myCombo)
+console.log(typeof myCombo)
+
+//Question 6
+let myOtherCombo = myNumber + myOtherNumber
+
+//Question 7
+let myRemainder = myNumber % 5
+console.log(myRemainder)
+
+//Question 8
+let i = 2
+
+//Question 9
+let int = 4
+
+//Question 10
+let myBoolean = false
